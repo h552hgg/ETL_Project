e5ef6db8b167c28314da50a325cf1fd12cba67af
h552hgg <h552hgg@gmail.com>
2021-01-10 12:11:01 -0600

    Final add

--- Resources/ETL_Project.sql
+++ Resources/ETL_Project.sql
@@ -1,36 +1,21 @@
 --Create Tables
 CREATE TABLE bitcoin ( 
 Date DATE PRIMARY KEY,
-CBBTCUSD money
+bit_price FLOAT
 );
-
 
 CREATE TABLE ethereum ( 
 date DATE PRIMARY KEY,
-eth_price money
+eth_price FLOAT
 );
 
---Verify tables are created and data entered
+--Verify Tables are created and import of CSV
+SELECT *
+From bitcoin;
+
 SELECT *
 From ethereum;
 
-SELECT *
-From bitcoin;
 
--- Change Column name in bitcoin database
-ALTER TABLE bitcoin 
-RENAME cbbtcusd TO bit_price;
 
-SELECT *
-From bitcoin;
 
---Tried to merge both tables while creating new crypto table
-DROP TABLE crypto;
-
-CREATE TABLE crypto as
-SELECT date,bit_price FROM bitcoin
-UNION ALL
-SELECT date,eth_price FROM ethereum;
-
-SELECT *
-FROM crypto;
